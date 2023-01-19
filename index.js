563838425fe925d938d3bccca4d18eaefd87df0d
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2023-01-19 17:21:55 +0600

    sofis first space created

--- index.js
+++ index.js
@@ -1,29 +1,93 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+require("colors");
+
+
+
 
 //middle wares
 app.use(cors());
 app.use(express.json());
 
 
-async function run(){
-    try{
+
+
+
+
+async function run() {
+
+
+
+
+
+
+
+
+
+
+    try {
+
+
+        //sofia start-----------
+
+
+        //sofia end------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
-    finally{
+    catch (error) {
 
     }
+    finally {
+
+    }
+
 }
-run().catch(err =>console.error(err))
 
-app.get('/', (req,res)=>{
+run().catch(err => console.error(`this error is from catch section ${err}`.bgRed))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
     res.send('GitFair server is up and running');
 })
 
-app.listen(port, ()=>{
-    console.log(`server is ruuning at port ${port}`)
+app.listen(port, () => {
+    console.log(`server is ruuning at port ${port}`.bgCyan)
 })
