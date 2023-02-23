23cb6df884150eedd08500aca5e25f2ca00e70d5
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2023-02-24 05:21:35 +0600

    added ssl commerz code

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ const client = new MongoClient(uri, {
 
 // STRIPE ---
 const stripe = require("stripe")(process.env.STRIPT_SECRET);
+//ssl commerz ---
+const SSLCommerzPayment = require('sslcommerz-lts')
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.STORE_PASSWORD
+const is_live = false //true for live, false for sandbox
+
 
 async function run() {
   try {
@@ -175,6 +181,58 @@ async function run() {
         });
       }
     });
+
+
+    // bkash paymetn start --
+    app.post("/pay-sslcommerz", (req, res) => {
+      const payConfirmUserDb = req.body;
+      console.log("iiiiiiiii:", payConfirmUserDb);
+      // const data = {
+      //   total_amount: 100,
+      //   currency: 'BDT',
+      //   tran_id: 'REF123', // use unique tran_id for each api call
+      //   success_url: 'http://localhost:3030/success',
+      //   fail_url: 'http://localhost:3030/fail',
+      //   cancel_url: 'http://localhost:3030/cancel',
+      //   ipn_url: 'http://localhost:3030/ipn',
+      //   shipping_method: 'Courier',
+      //   product_name: 'Computer.',
+      //   product_category: 'Electronic',
+      //   product_profile: 'general',
+      //   cus_name: 'Customer Name',
+      //   cus_email: 'customer@example.com',
+      //   cus_add1: 'Dhaka',
+      //   cus_add2: 'Dhaka',
+      //   cus_city: 'Dhaka',
+      //   cus_state: 'Dhaka',
+      //   cus_postcode: '1000',
+      //   cus_country: 'Bangladesh',
+      //   cus_phone: '01711111111',
+      //   cus_fax: '01711111111',
+      //   ship_name: 'Customer Name',
+      //   ship_add1: 'Dhaka',
+      //   ship_add2: 'Dhaka',
+      //   ship_city: 'Dhaka',
+      //   ship_state: 'Dhaka',
+      //   ship_postcode: 1000,
+      //   ship_country: 'Bangladesh',
+      // };
+      // const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+      // sslcz.init(data).then(apiResponse => {
+      //   // Redirect the user to payment gateway
+      //   let GatewayPageURL = apiResponse.GatewayPageURL
+      //   res.redirect(GatewayPageURL)
+      //   console.log('Redirecting to: ', GatewayPageURL)
+      // });
+    })
+    // bkash payment end ---
+
+
+
+
+
+
+
 
     // post users info 
     app.post('/users', async (req, res) => {
